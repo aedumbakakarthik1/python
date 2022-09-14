e628ced459bc3bea3bcec9b98cfd655d52c24bc0
karthik <karthikaedumbaka1@gmail.com>
2022-09-14 12:44:37 +0530

    python day-1

---
+++ 1)helloWorld.py
@@ -0,0 +1,9 @@
+## to print hello world
+print("hello world!")
+
+# to add 2  number
+a,b=10,20
+print(a+b)
+"""in another programming language like java, c etc if we need to initialize them because there are statically Typed programming languages
+where as in python we don't need to initialize them because python we a dinamically typed programming language
+"""
---
+++ 1)helloworld.java
@@ -0,0 +1,18 @@
+// to print hello world
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+
+// ----------------------------------------------------------------
+// to add 2 numbers
+public class addition{
+    public static void main(String[] args) {
+        int a,b;
+        a=10;
+        b=20;
+        System.out.println(a+b);
+    }
+}
