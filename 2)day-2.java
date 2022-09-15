ca6db8ab6ab33188290ff63f1603c1ac8ff5a933
karthik <karthikaedumbaka1@gmail.com>
2022-09-15 16:15:47 +0530

    day-2

---
+++ 2)day-2.java
@@ -0,0 +1,11 @@
+// for loop in java
+
+public class addition{
+    public static void main(String[] args) {
+        int i;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+       
+        }
+    }
+}
---
+++ 2)day-2.py
@@ -0,0 +1,3 @@
+#for loop in python
+for i in range(10):
+    print(i,end=" ")
